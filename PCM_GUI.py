7e09d702d1792cd5deaef7719ce0a20325696380
MarkVestergaard <102877223+MarkVestergaard@users.noreply.github.com>
2022-04-03 17:54:13 +0200

    Add files via upload

--- PCM_GUI.py
+++ PCM_GUI.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 16 21:33:05 2021
+GUI for analyzing 2D phase contrast mapping (PCM) MRI data to measure trough-plane flow.
 
-GUI for drawing ROI on PCM - images and calculate Flow.
+Written for python 3.8. Tested on PCM data from 3T Philips dSTREAM Achieva MRI and 3T Siemens Biograph mMR hybrid PET/MR system.
+
+Input for Philips scanner data: **.PAR file
+Input for Siemens scanner data: nifti-file converted from dicom using dcmniix (https://github.com/rordenlab/dcm2niix)
+
+Region of interest (ROI) is manual delineated or automatically drawn based using region-growin algoritm. Data saved as csv file.
+
+For in-depth description of analysis see: Vestergaard et al. Cerebral Cortex, Volume 32, Issue 6, 15 March 2022, 1295–1306, doi:https://doi.org/10.1093/cercor/bhab294 or Vestergaard et al. Journal of Cerebral Blood Flow & Metabolism 2019, Vol. 39(5) 834–848, doi:https://doi.org/10.1177/0271678X17737909
+
+Mark B. Vestergaard
+Functional Imaging Unit,
+Department of Clinical Physiology and Nuclear Medicine
+Rigshospitalet Copenhagen, Denmark
+mark.bitsch.vestergaard@regionh.dk
 
 @author: Mark B. Vestegaard, June 2021, mark.bitsch.vestergaard@regionh.dk 
 """
